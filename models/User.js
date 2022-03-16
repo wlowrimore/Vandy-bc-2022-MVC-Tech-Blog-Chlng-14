c4630ef4cf724fd2dd829533b414e94b6ef76ffd
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-03-16 09:37:32 -0500

    adds user model and routes

--- models/User.js
+++ models/User.js
@@ -4,6 +4,9 @@ const {
 } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+const {
+  builtinModules
+} = require('module');
 
 // create User model
 class User extends Model {
@@ -15,6 +18,48 @@ class User extends Model {
 // create feilds/columns for User model
 User.init({
   id: {
-    type: DataType
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8]
+    }
   }
-})
+}, {
+  hooks: {
+    // beforeCreate lifecycle
+    async beforeCreate(newUserData) {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
+    },
+    // beforeUpdate lifecycle
+    async beforeUpdate(updatedUserData) {
+      updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+      return updatedUserData;
+    }
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'user'
+});
+
+module.exports = User;
