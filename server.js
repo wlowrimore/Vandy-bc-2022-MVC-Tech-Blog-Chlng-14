db1044fbf64ae3500951ed85e436d5b5418b598c
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-03-25 07:48:39 -0500

    adds timeout

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
+  cookie: {
+    expires: 120000
+  },
+  resave: true,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
